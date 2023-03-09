0642668ff4d5fa5b5f31ea448238aff1e79239a9
Kyle Williams <15004488+elykkyle@users.noreply.github.com>
2023-03-09 14:21:04 -0600

    add database logging

--- index-ws.js
+++ index-ws.js
@@ -16,6 +16,14 @@ server.listen(PORT, function () {
   console.log('Listening on ' + PORT);
 });
 
+process.on('SIGINT', () => {
+  console.log('sigint');
+  wss.clients.forEach((client) => client.close());
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 const wss = new WebSocketServer({ server: server });
 
 wss.on('connection', function connection(ws) {
@@ -27,6 +35,10 @@ wss.on('connection', function connection(ws) {
   if (ws.readyState === ws.OPEN) {
     ws.send('Welcome to my server');
   }
+
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+  `);
 
   ws.on('close', function close() {
     wss.broadcast(`Current visitors: ${numClients}`);
@@ -41,3 +53,29 @@ wss.broadcast = function broadcast(data) {
 };
 
 // Websockets end
+
+/* Begin DATABASE */
+
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `);
+});
+
+function getCounts() {
+  db.each('SELECT * FROM visitors', (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log('shutting down database');
+  db.close();
+}
